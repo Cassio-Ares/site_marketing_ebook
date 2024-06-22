c1f6c30599b74f116061c1508122fd77028a6c13
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-06-22 14:41:20 -0300

    back

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const fs = require("fs"); //
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/register", (_, res) => {
+  const file = fs.readFileSync("./database/register.json");
+
+  const object = JSON.parse(file);
+
+  res.send(object);
+});
+
+router.post("/salvarRegister", (req, res) => {
+  const data = req.body;
+
+  const email = data.email;
+  const telefone = data.telefone;
+
+  const file = fs.readFileSync("./database/register.json");
+
+  const object = JSON.parse(file);
+
+  object.push({
+    email: email,
+    telefone: telefone,
+  });
+
+  const string = JSON.stringify(object);
+
+  fs.writeFileSync('./database/register.json', string);
+  
+
+  res.send("Parabens pelo cadastro");
 });
 
 module.exports = router;
